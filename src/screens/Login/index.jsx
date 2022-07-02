636e97484f213788e6be8aef371cb0b0e801985b
nester7x <nester.letopisets@gmail.com>
2022-07-02 16:06:19 +0300

    login page

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,10 +1,117 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './styles';
 
-const Login = () => (
-  <S.Wrap>
-    <S.Title>Login</S.Title>
-  </S.Wrap>
-);
+function getCookie(cname) {
+  const name = `${cname}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(';');
+  for (let i = 0; i < ca.length; i += 1) {
+    let c = ca[i];
+    while (c.charAt(0) === '') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
+
+let initialToken;
+let submitted;
+
+if (getCookie('token') === '' || getCookie('token') === 'null') {
+  initialToken = null;
+  submitted = false;
+} else {
+  initialToken = getCookie('token');
+  submitted = true;
+}
+
+const Login = () => {
+  const [token, setToken] = useState(initialToken);
+  const [emailValue, setEmailValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(submitted);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (token !== null) {
+      setIsSubmitted(true);
+    }
+
+    document.cookie = `token=${token}; expires=${new Date()}; path=/`;
+  }, [token]);
+
+  const handleEmailChange = (event) => {
+    setEmailValue(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPasswordValue(event.target.value);
+  };
+
+  const getToken = async (user) => {
+    const response = await fetch(
+      'https://nestbe.herokuapp.com/api/user/login',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+      }
+    );
+    if (!response.ok) {
+      setError('Wrong login or password...');
+    }
+
+    const data = await response.json();
+    return data.user.token;
+  };
+
+  const checkData = (event) => {
+    event.preventDefault();
+
+    getToken({
+      email: `${emailValue}`,
+      password: `${passwordValue}`
+    }).then((value) => setToken(value));
+  };
+
+  const renderForm = (
+    <S.Wrap>
+      <S.DataForm>
+        <S.ErrorMessage>{error}</S.ErrorMessage>
+        <S.DataInput
+          value={emailValue}
+          onChange={handleEmailChange}
+          placeholder="email..."
+        />
+        <S.DataInput
+          type="password"
+          value={passwordValue}
+          onChange={handlePasswordChange}
+          placeholder="password..."
+        />
+        <S.LoginBtn type="submit" onClick={checkData}>
+          Login
+        </S.LoginBtn>
+      </S.DataForm>
+    </S.Wrap>
+  );
+
+  return (
+    <div>
+      {isSubmitted ? (
+        <S.Wrap>
+          <S.MessageWrapper>Authorized successfully</S.MessageWrapper>
+        </S.Wrap>
+      ) : (
+        renderForm
+      )}
+    </div>
+  );
+};
 
 export default Login;
--- src/screens/Login/styles.jsx
+++ src/screens/Login/styles.jsx
@@ -1,10 +1,76 @@
 import styled from 'styled-components';
+import Btn from 'components/Button';
+import Input from 'components/Input';
 
 export const Wrap = styled.div`
-  background-color: #ccc;
-  padding: 0 15px;
+  background-color: #2148c0;
+  padding: 15px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const Title = styled.h1`
-  color: blue;
+export const DataForm = styled.form`
+  width: 250px;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+
+export const ErrorMessage = styled.p`
+  margin: 0 auto 20px;
+  color: red;
+  font-size: 18px;
+`;
+
+export const DataInput = styled(Input)`
+  padding: 10px 15px;
+  border: 1px solid #ffffff;
+  border-radius: 6px;
+  background: transparent;
+  width: 100%;
+  color: #ffffff;
+  margin: 0 0 10px;
+  :focus {
+    outline: none;
+  }
+  ::placeholder {
+    color: rgba(255, 255, 255, 0.8);
+  }
+`;
+
+export const LoginBtn = styled(Btn)`
+  color: #264eca;
+  background-color: #ffffff;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.3);
+  border-radius: 4px;
+  width: 100%;
+  transition: 0.3s;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+export const MessageWrapper = styled.div`
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  height: 150px;
+  border-radius: 4px;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #2148c0;
+  padding: 15px;
+  font-size: 25px;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.3);
 `;
